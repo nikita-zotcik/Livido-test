b7c17cad4edddc358e6197c8558eb7b4c88954d3
evgeny-kholodov <evgeny.exceed@gmail.com>
2018-06-22 13:33:55 +0300

    added test buttons

--- src/components/flow/flow.js
+++ src/components/flow/flow.js
@@ -4,15 +4,33 @@ import { Aside } from './aside/aside';
 
 class Flow extends Component {
 	state = {
-		stepNumber: 5,
+		stepNumber: 1,
 		steps: ['Opret konto', 'Tilknyt regnskabssystem', 'Design kampagne', 'Vælg sager', 'Bekræft kampagne' ]
 	};
+
+	nextStep = () => {
+		let stepNumber = this.state.stepNumber;
+		if (stepNumber === 6) return;
+		stepNumber+= 0.5;
+		console.log('stepNumber',stepNumber);
+		this.setState({stepNumber});
+	}
+
+	prevStep = () => {
+		let stepNumber = this.state.stepNumber;
+		if (stepNumber === 1) return;
+		stepNumber-= 0.5;
+		this.setState({stepNumber});
+	}
 	render() {
 		return (
 			<div className={'flow'}>
 				<Stepper steps={this.state.steps} stepNumber={this.state.stepNumber}/>
 				<div className={'main-part'}>
 					<div className={'main-block'}>
+						<h1>Test step-by-step</h1>
+						<button onClick={this.prevStep}>Prev Step</button>
+						<button onClick={this.nextStep}>Next Step</button>
 					</div>
 					<Aside stepNumber={this.state.stepNumber}/>
 				</div>
